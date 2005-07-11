f9963fe878af225693aa114552b75ce7b772b201
JoungKyun Kim <joungkyun@daum.net>
2005-07-11 05:33:17 +0000

    add new package build script

---
+++ esystem.pack.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ver=$(cat version)
+tpp=$$
+
+tmp=/tmp/eSys.${tpp}
+
+mkdir -p ${tmp}/eSystem-${ver}
+cp -rf eSystem/ eSystem.php ${tmp}/eSystem-${ver}/
+cp -rf package.xml ${tmp}/
+
+pushd ${tmp}
+find ./ -name CVS -exec rm -rf {} \;
+tar cvfpz eSystem-${ver}.tgz eSystem-${ver} package.xml
+popd
+
+mv ${tmp}/eSyste-${ver}.tgz ./
+rm -rf ${tmp}
