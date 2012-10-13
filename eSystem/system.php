08ce7f2363b6ee0f2a537a620d2d4837586dc7d3
JoungKyun Kim <joungkyun@daum.net>
2012-10-13 20:11:40 +0000

    changed tagging naming

--- eSystem/system.php
+++ eSystem/system.php
@@ -1,47 +1,34 @@
 <?php
-/**
- * Project: eSystem:: The Extended file system<br>
- * File:    eSystem/system.php
- *
- * Sub pcakge of eSystem package. This package includes extended system
- * methods.
- *
- * @category   System
- * @package    eSystem
- * @subpackage eSystem_system
- * @author     JoungKyun.Kim <http://oops.org>
- * @copyright  (c) 2009, JoungKyun.Kim
- * @license    BSD
- * @version    $Id$
- * @link       http://pear.oops.org/package/KSC5601
- * @filesource
- */
+//
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2003 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.02 of the PHP license,      |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/2_02.txt.                                 |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Author: JoungKyun.Kim <http://oops.org>                              |
+// +----------------------------------------------------------------------+
+//
+// $Id: system.php,v 1.8 2007-03-03 16:25:23 oops Exp $
 
-/**
- * alternative sysem class that based eSystem class
- *
- * @package eSystem
- */
 class eSystem_system
 {
-	// {{{ properties
-	public $tmpdir = '/tmp';
-	public $_stdout;
-	public $_stderr;
-	public $_retint = 0;
+	var $tmpdir = '/tmp';
+	var $tmpname = 'eSystem_system_';
+	var $_stdout;
+	var $_stderr;
+	var $_retint = 0;
 
-	private $tmpname = 'eSystem_system_';
-	// }}}
-
-	// {{{ function _system ($_cmd, $_out = 0)
-	/**
-	 *
-	 * Proto method of eSystem exec functions.
-	 *
-	 * @access public
-	 * @return void
-	 * @param  string command that execute an external program and display the output
-	 * @param  int    whether saving output message on self::$_stdout
+	/*
+	 * define origin proto function
+	 * start function name _
 	 */
 	function _system ($_cmd, $_out = 0) {
 		$_err = tempnam ($this->tmpdir, $this->tmpname);
@@ -81,7 +68,6 @@ class eSystem_system
 		endif;
 		unlink ($_err);
 	}
-	// }}}
 }
 
 /*
